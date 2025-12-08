b88b764f687b3229e94d23f44d1c4764c76c218f
Pierre-Clément Kerneïs <pc.kerneis@gmail.com>
2025-12-08 18:16:46 +0100

    Update next.config.js: Remove output export

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const repoBasePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
 
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
   ...(repoBasePath
     ? {
         basePath: `/${repoBasePath}`,
