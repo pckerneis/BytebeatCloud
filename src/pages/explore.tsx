637f91fcd5935143efd63d33c889b69658da37eb
pckerneis <pc.kerneis@gmail.com>
2025-12-24 11:30:23 +0100

    Fix missing favorited_by_current_user property

--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -222,7 +222,7 @@ export default function ExplorePage() {
             const result = await supabase
               .from('posts_with_meta')
               .select(
-                'id,title,expression,sample_rate,mode,created_at,profile_id,is_draft,fork_of_post_id,is_fork,author_username,origin_title,origin_username,favorites_count,is_weekly_winner,license,comments_count',
+                'id,title,expression,sample_rate,mode,created_at,profile_id,is_draft,fork_of_post_id,is_fork,author_username,origin_title,origin_username,favorites_count,favorited_by_current_user,is_weekly_winner,license,comments_count',
               )
               .in('id', ids);
 
@@ -259,7 +259,7 @@ export default function ExplorePage() {
         const result = await supabase
           .from('posts_with_meta')
           .select(
-            'id,title,expression,sample_rate,mode,created_at,profile_id,is_draft,fork_of_post_id,is_fork,author_username,origin_title,origin_username,favorites_count,is_weekly_winner,license,comments_count',
+            'id,title,expression,sample_rate,mode,created_at,profile_id,is_draft,fork_of_post_id,is_fork,author_username,origin_title,origin_username,favorites_count,favorited_by_current_user,is_weekly_winner,license,comments_count',
           )
           .eq('is_draft', false)
           .order('created_at', { ascending: false })
